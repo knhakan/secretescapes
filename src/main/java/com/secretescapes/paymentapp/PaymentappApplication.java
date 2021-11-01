9a8467065784aeaaaf89306a5e82b0bf067b453f
knhakan <knhakan@gmail.com>
2021-11-01 22:34:39 +0100

    Welcoming message and needed Jpa annotation have been added

--- src/main/java/com/secretescapes/paymentapp/PaymentappApplication.java
+++ src/main/java/com/secretescapes/paymentapp/PaymentappApplication.java
@@ -2,13 +2,15 @@ package com.secretescapes.paymentapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PaymentappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentappApplication.class, args);
-        System.out.println("av");
+        System.out.println("Welcome to Secret Escapes Payment application!");
     }
 
 }
